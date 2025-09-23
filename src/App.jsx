9a0c85f30001b543011b246dce524a4b229225df
Lia Matsubara <matsubaralia@gmail.com>
2025-09-22 21:25:36 -0300

    feat(p1): cria componente principal e faz hello

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <div className="container px-2 mt-4">
+
+      {/* lista */}
+      <div className="row">
+        <div className="col-12 d-flex">
+          <div
+            className="flex-grow-1 text-center">
+            Hello, Lembretes</div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
